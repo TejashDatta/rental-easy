bbd691388ea5f0fd364cb798fb5651e5a516f779
Tejash <tejashdatta@gmail.com>
2020-01-16 20:54:15 +0530

    radr_logo => table talk logo

--- pages/questions/talk.vue
+++ pages/questions/talk.vue
@@ -1,26 +1,44 @@
 <template>
   <v-container v-if="done">
     <div class="d-flex flex-column align-center my-12 pa-2">
-      <img src="~/assets/radr_logo.png" class="my-10" />
-      <v-btn big @click="book" color="pink" dark class="my-10" v-if="!booked">Book</v-btn>
-      <p
-        class="my-10"
-        v-if="!booked"
-      >Click on book and we'll find your perfect match and get back to you through email.</p>
-      <p class="my-12" v-if="booked">Thank you for your interest! We'll get back to you shortly.</p>
+      <img src="~/assets/table_talk_logo.png" class="my-10" />
+      <v-btn big @click="book" color="pink" dark class="my-10" v-if="!booked"
+        >Book</v-btn
+      >
+      <p class="my-10" v-if="!booked">
+        Click on book and we'll find your perfect match and get back to you
+        through email.
+      </p>
+      <p class="my-12" v-if="booked">
+        Thank you for your interest! We'll get back to you shortly.
+      </p>
     </div>
   </v-container>
   <v-container v-else>
     <h2>Tell us about yourself</h2>
-    <p class="my-8">Answer the questions below to help us find the people most suitable for you.</p>
+    <p class="my-8">
+      Answer the questions below to help us find the people most suitable for
+      you.
+    </p>
     <v-row>
-      <v-col cols="6" sm="4" lg="3" v-for="(question, i) in questions" :key="question.options[0]">
+      <v-col
+        cols="6"
+        sm="4"
+        lg="3"
+        v-for="(question, i) in questions"
+        :key="question.options[0]"
+      >
         <v-radio-group v-model="answers[i]" column class="pa-0 mx-0 my-4">
           <span class="mb-2 ml-1">
-            <strong>{{i+1}}.</strong>
-            {{question.q}}
+            <strong>{{ i + 1 }}.</strong>
+            {{ question.q }}
           </span>
-          <v-radio v-for="option in question.options" :key="option" :label="option" :value="option"></v-radio>
+          <v-radio
+            v-for="option in question.options"
+            :key="option"
+            :label="option"
+            :value="option"
+          ></v-radio>
         </v-radio-group>
       </v-col>
     </v-row>
@@ -31,7 +49,8 @@
         class="my-12"
         @click="submit"
         :disabled="answers.includes(undefined)"
-      >Continue</v-btn>
+        >Continue</v-btn
+      >
     </div>
   </v-container>
 </template>
